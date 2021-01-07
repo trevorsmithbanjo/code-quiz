b95019ff9d0767c262d575f75ef1634b3161be37
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-06 21:49:13 -0600

    Started quiz questions function. Going to hardcode quiz area instead due to many issues.

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ var userScore = 0;
 var count = 60;
 timer.textContent = "Timer: " + count;
 
+var questions = [
+    { q: "if you run this code what do you expect to see the output?\n let x = 20;\n for(let i = 0; i< 20; i++) {\nx = x + 2}", a: ["A: 40", "B: 444", "C: 60", "D: 0"] },
+];
+
+// Buttons event listener
+quizArea.addEventListener("click", function (e) {
+    var target = e.target;
+    console.log(target);
+    console.log(e);
+    if (target.matches("button") === true) {
+        clearQuiz();
+        quizQuestions();
+        setTimer();
+    }
+})
+
 // Function to set timer
 function setTimer() {
     var timerInterval = setInterval(function () {
@@ -40,15 +56,26 @@ function clearQuiz() {
     }
 }
 
-// Buttons event listener
-quizArea.addEventListener("click", function (e) {
-    var target = e.target;
-    console.log(target);
-    console.log(e);
-    if (target.matches("button") === true) {
-        clearQuiz();
-        setTimer();
-    }
-})
+// Quiz questions function
+function quizQuestions() {
+    for (i = 0; i < questions.length; i++) {
+        var question = questions[i];
+        var questionH2 = document.createElement("h2");
+        questionH2.textContent = question.q;
+        quizArea.appendChild(questionH2);
 
-quizStart();
+        for (j = 0; j < question.a.length; j++) {
+            var answer = question.a[j];
+            var answerDiv = document.createElement("div");
+            var answersBtn = document.createElement("button");
+            answersBtn.textContent = answer;
+            answersBtn.classList.add("btn", "btn-info");
+            answerDiv.append(answersBtn);
+            quizArea.append(answerDiv);
+            console.log(answer);
+        }
+    }
+}
+
+// quizStart();
+quizQuestions();
