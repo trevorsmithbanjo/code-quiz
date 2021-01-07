1d0a3a63ef1963489cd5ecd695a94a89cdf98302
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-07 08:15:26 -0600

    Linked jQuery to get dynamic display to work properly.

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Variables for elements to be modified
-var highScore = document.getElementById("high-score");
-var timer = document.getElementById("timer");
-var quizArea = document.getElementById("quiz-area");
+var highScore = $("#high-score");
+var timer = $("#timer");
+var quizArea = $("#quiz-area");
+var quizTitle = $("#quiz-title");
+var quizQuestion = $("#quiz-question");
+var startQuiz = $("#start-quiz");
 
 // Variables to change state
 var userScore = 0;
 var count = 60;
 timer.textContent = "Timer: " + count;
 
+// object to hold questions
 var questions = [
-    { q: "if you run this code what do you expect to see the output?\n let x = 20;\n for(let i = 0; i< 20; i++) {\nx = x + 2}", a: ["A: 40", "B: 444", "C: 60", "D: 0"] },
+    {
+        q: "This is a question",
+        a: ["A", "B", "C", "D"],
+        c: "C"
+    },
 ];
 
-// Buttons event listener
-quizArea.addEventListener("click", function (e) {
-    var target = e.target;
-    console.log(target);
-    console.log(e);
-    if (target.matches("button") === true) {
-        clearQuiz();
-        quizQuestions();
-        setTimer();
-    }
+// Start quiz
+startQuiz.on("click", function () {
+    startQuiz.hide();
+    quizTitle.hide();
 })
 
 // Function to set timer
@@ -34,26 +36,6 @@ function setTimer() {
             clearInterval(timerInterval);
         }
     }, 1000)
-}
-
-// Funcion to populate quiz area
-function quizStart() {
-    var codeH1 = document.createElement("h1");
-    codeH1.textContent = "Code Quiz Challenge"
-    quizArea.appendChild(codeH1);
-    var startBtn = document.createElement("button");
-    startBtn.textContent = "Start Quiz";
-    startBtn.classList.add("btn", "btn-info");
-    quizArea.appendChild(startBtn);
-}
-
-// Clear quiz area function
-function clearQuiz() {
-    var child = quizArea.lastElementChild;
-    while (child) {
-        quizArea.removeChild(child);
-        child = quizArea.lastElementChild;
-    }
 }
 
 // Quiz questions function
@@ -76,6 +58,3 @@ function quizQuestions() {
         }
     }
 }
-
-// quizStart();
-quizQuestions();
