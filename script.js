270b1f5e2de7804e0982d6a93464126402999975
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-12 22:29:35 -0600

    Game Functions With A Few Bugs To Fix.

--- script.js
+++ script.js
@@ -12,19 +12,44 @@ $(document).ready(function () {
     var count = 60;
     var questionCount = 0;
     timer.textContent = "Timer: " + count;
-    var userName = "";
+    var data = [];
+
+    // Check for local storage
+    function checkStorage() {
+        var storedData = JSON.parse(localStorage.getItem("data"));
+
+        if (storedData !== null) {
+            data = storedData;
+        }
+    }
+    checkStorage();
 
     // object to hold questions
     var questions = [
         {
-            q: "This is a question",
-            a: ["A", "B", "C", "D"],
-            c: "C"
+            q: "An array is used to __?",
+            a: ["Stop a function", "Win at life", "Store multiple values in a single variable", "Store one value in a single variable"],
+            c: "Store multiple values in a single variable"
         },
         {
-            q: "This is another question",
-            a: ["A", "B", "C", "D"],
-            c: "A"
+            q: "2 + \"2\" = ",
+            a: ["4", "22", "\"\"", "\"22\""],
+            c: "\"22\""
+        },
+        {
+            q: "What method can loop through an array?",
+            a: ["slice()", "forEach()", "logarithm", "push()"],
+            c: "forEach()"
+        },
+        {
+            q: "The window is?",
+            a: ["An object", "A pane of glass", "A computer screen", "None of the above"],
+            c: "An object"
+        },
+        {
+            q: "How long did it take Brendan Eich to develop Javascript?",
+            a: ["2 months", "10 days", "10 years", "2 weeks"],
+            c: "10 days"
         },
     ];
     var question = questions[questionCount];
@@ -50,6 +75,7 @@ $(document).ready(function () {
 
             if (count == 0) {
                 clearInterval(timerInterval);
+                gameOver();
             }
         }, 1000)
     }
@@ -78,6 +104,7 @@ $(document).ready(function () {
 
     // New Question function
     function newQuestion() {
+        console.log("answers" + question.a);
         // Check if any questions left in game.
         if (questionCount <= questions.length - 1) {
             quizArea.empty();
@@ -86,7 +113,7 @@ $(document).ready(function () {
 
             // Appends answer buttons to quizArea
             for (i = 0; i < question.a.length; i++) {
-                var answer = question.a[i];
+                var answer = questions[questionCount].a[i];
                 var answerBtn = $(`<div><button class="btn btn-info p-2 mb-2 answerBtn" value="${answer}">${answer}</button></div>`)
                 quizArea.append(answerBtn);
             }
@@ -114,14 +141,39 @@ $(document).ready(function () {
         $("#name-btn").on("click", function (event) {
             event.preventDefault();
             var userName = $("#user-name").val().trim();
-            localStorage.setItem("name", userName);
-            localStorage.setItem("score", userScore);
+            var userData = {
+                name: userName,
+                score: userScore
+            };
+            data.push(userData);
+            localStorage.setItem("data", JSON.stringify(data));
+            displayScores();
         })
     }
 
-    var savedScores = localStorage.getItem("name");
-    var savedNames = localStorage.getItem("score");
-    console.log(savedScores);
-    console.log(savedNames);
+    // Function to display high scores
+    function displayScores() {
+        quizArea.append(`<div><h3>High Score</h3></div>
+                        <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Score</th>
+                            </tr>
+                        </thead>`);
+        for (i = 0; i < data.length; i++) {
+            var name = data[i].name;
+            var score = data[i].score;
+            $(".table").append(`<tbody>
+                                    <tr>
+                                        <td>${name}</td>
+                                        <td>${score}</td>
+                                    </tr>
+                                <tbody>`)
+        }
+    }
 
+    highScore.on("click", function () {
+        displayScores();
+    });
 })
